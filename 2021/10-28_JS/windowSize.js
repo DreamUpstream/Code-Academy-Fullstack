204c30204a1f6f6c7465f798bd8272705bd5c18c
DreamUpstream <gabrielius1621@gmail.com>
2021-10-28 18:12:22 +0300

    10-28

---
+++ 2021/10-28_JS/windowSize.js
@@ -0,0 +1,9 @@
+window.addEventListener('resize', function(event){
+    
+let w = window.innerWidth;
+let h = window.innerHeight;
+document.getElementById('windowSize').innerText = 'Lango aukštis: ' + h + ' Lango plotis:' + w;
+});
+let w = window.innerWidth;
+let h = window.innerHeight;
+document.getElementById('windowSize').innerText = 'Lango aukštis: ' + h + ' Lango plotis:' + w;
