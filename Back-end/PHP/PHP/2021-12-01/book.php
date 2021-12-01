7444363775fccadeacec222d831af7633e133737
DreamUpstream <gabrielius1621@gmail.com>
2021-12-01 21:32:08 +0200

    .

---
+++ Back-end/PHP/PHP/2021-12-01/book.php
@@ -0,0 +1,36 @@
+<?php
+
+function vardumpBooks($books) {
+    foreach ($books as $key => $value) {
+        foreach ($value as $subValue) {
+            var_dump($subValue);
+        }
+    }
+}
+
+function bookYearAvarage($books) {
+    $avarageSum = 0;
+    $avarageAmount = 0;
+    foreach ($books as $key => $value) {
+        if ($key = 'Year') {
+            foreach ($value as $subValue) {   
+                if (is_int($subValue)) {
+                    $avarageSum += $subValue; 
+                    $avarageAmount++;
+                }
+            }
+        }
+    }
+    return $avarageSum/$avarageAmount;
+}
+
+$books = [
+    'Title' => ['Trys parseliai', 'Haris poteris', 'Mazasis princas'],
+    'Author' => ['Jonas Kazimieras', 'Astrida Lindgren', 'Foo Baris'],
+    'Year' => [1990, 1999, 2005],
+    'Genre' => ['Children literature', 'Fantasy', 'Science fiction'],
+];
+
+vardumpBooks($books);
+echo '<br><br>';
+echo bookYearAvarage($books);
