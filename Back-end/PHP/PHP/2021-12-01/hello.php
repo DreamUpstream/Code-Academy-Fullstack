012cdc0699e052cad3846aac17458223d4502e22
DreamUpstream <gabrielius1621@gmail.com>
2021-12-01 18:49:16 +0200

    .

--- Back-end/PHP/PHP/2021-12-01/hello.php
+++ Back-end/PHP/PHP/2021-12-01/hello.php
@@ -1,4 +1,31 @@
+<html>
+<body>
+<h1>Enter word to find if it has h, e or o:</h1>
+
+<form action="" method="post">
+  Word: <input name="example" type="text" />
+  <input name="submit" type="submit" />
+</form>
+</body>
+</html>
 <?php
+    echo 'Example 1: <br>';
     $hello = 'hello';
-    echo "\"$hello[0]\", \"$hello[1]\", \"$hello[-1]\".";
+    echo "\"$hello[0]\", \"$hello[1]\", \"$hello[-1]\". <br>";
+    echo 'Example 2: <br>';
+    function letterWord($word, $letter) {
+        if (strpos($word, $letter) !== false)
+        {
+            return "$word contains $letter <br>";
+        }
+        else {
+            return "$word does not contain $letter <br>";
+        }
+    }
+    if (isset($_POST['submit'])) {
+        $word = $_POST['example'];
+        echo "You entered $word. <br>";
+        echo letterWord($word, 'h').letterWord($word, 'e').letterWord($word, 'o');
+      }
 ?>
+    
