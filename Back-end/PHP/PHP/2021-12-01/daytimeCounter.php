e2e56920f5d3e9bda3f4e79ce060c64971913971
DreamUpstream <gabrielius1621@gmail.com>
2021-12-01 18:32:12 +0200

    .

---
+++ Back-end/PHP/PHP/2021-12-01/daytimeCounter.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<h1>Enter the amount of days:</h1>
+
+<form action="" method="post">
+  Example value: <input name="example" type="text" />
+  <input name="submit" type="submit" />
+</form>
+</body>
+</html>
+<?php
+function seconds2human($d) {
+    $s = $d*60*60*24;
+    
+    return "$d days have $s seconds in it.";
+    }
+
+    if (isset($_POST['submit'])) {
+        $example = $_POST['example'];
+        echo seconds2human((integer)$example);
+      }
+?>
