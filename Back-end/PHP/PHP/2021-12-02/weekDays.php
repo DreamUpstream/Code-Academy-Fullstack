848ced4a8924dec9ca2d7a578801a4c03c6e7ceb
DreamUpstream <gabrielius1621@gmail.com>
2021-12-02 18:53:49 +0200

    .

--- Back-end/PHP/PHP/2021-12-02/weekDays.php
+++ Back-end/PHP/PHP/2021-12-02/weekDays.php
@@ -3,17 +3,36 @@ $arr = [
     'lt' => ['Pirmadienis', 'Antradienis', 'Treciadienis', 'Ketvirtadienis', 'Penktadienis', 'Sestadienis', 'Sekmadienis'],
     'en' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 ];
-echo "{$arr['lt'][0]}, {$arr['en'][2]} <br>";
-$lang = 'lt';
+echo "{$arr['lt'][0]}, {$arr['en'][2]} <br> <br>";
+?>
+
+<html>
+<body>
+<h3>Enter your preferred language to show current day:</h1>
+
+<form action="" method="post">
+  Language: <input name="weekDay" type="text" />
+  <input name="submit" type="submit" />
+</form>
+</body>
+</html>
+
+<?php
 function dayPrinter ($arr, $lang) {
     $today = date("l");
     if ($lang == 'en') {
-        echo "Today's date is $today";
+        echo "Today's weekday is $today";
     }
-    else {
+    else if ($lang == 'lt'){
         echo 'Šiandien yra ';
         $index = array_search($today, $arr['en']);
         echo $arr['lt'][3];
     }
+    else {
+        echo 'You have to enter \'en\' or \'lt\'';
+    }
 }
-dayPrinter($arr, $lang);
+if (isset($_POST['submit'])) {
+    $lang = $_POST['weekDay'];
+    dayPrinter($arr, $lang);
+}
