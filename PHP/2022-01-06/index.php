f3b629a1ad3340e0887f3de43afaa15ed341c309
DreamUpstream <gabrielius1621@gmail.com>
2022-01-06 20:54:10 +0200

    tagBuilder

--- PHP/2022-01-06/index.php
+++ PHP/2022-01-06/index.php
@@ -7,8 +7,7 @@ $tag = new Tag ('a');
 
 $tag->setText('title')->setAttr('href', 'index.html')->show();
 
-$tag2 = new Tag ('br');
+$tag2 = (new Tag ('br'))->show();
 
-$tag2 -> show();
 
 echo $tag->setText('title')->setAttr('href', 'index.html')->get();
